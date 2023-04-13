c7166dfe9e1740b115f88d8bb37da865905da65d
Thetoastinside <127579702+Thetoastinside@users.noreply.github.com>
2023-04-13 16:03:07 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # javascript-quiz
+
+## Author: Ryan Anderson
+
+## Deployment:
+
+
+### User Story:
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+
+### Screenshots:
+
+
+
+### Sources:
+
+html, css, and javascript help from https://www.youtube.com/@WebDevSimplified
---
+++ script.js
@@ -0,0 +1,27 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById
+('question-container')
+
+const shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setnextQuestion()
+}
+
+function setnextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [
+    {
+        question: 
+    }
+]
